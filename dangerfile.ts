cba196ececa7fd6d51dc54b1e10bbc98cfdf7486
Genilton Souza <genilton.souza26@gmail.com>
2023-04-03 18:07:00 -0400

    chore(dangerfile.ts): add fail config

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,7 +1,8 @@
-import { danger, warn, fail } from "danger";
+import { danger, warn, fail, message } from "danger";
 
 //get modified files with danger.git.modified_files
 const modifiedFiles = danger.git.modified_files;
+message(`Modified files: ${modifiedFiles.join(", ")}`);
 
 //get files without unit test files
 const filesWithoutTest = modifiedFiles.filter(
@@ -12,4 +13,9 @@ if (filesWithoutTest.length > 0) {
   warn(
     `You have modified files without test files: ${filesWithoutTest.join(", ")}`
   );
+}
+
+// pr with no description
+if (danger.github.pr.body && danger.github.pr.body.length < 10) {
+  fail("Please provide a proper PR description.");
 }
